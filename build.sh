9412da6de2e1b9cd87d2b9333c54796534915a08
syusaku625 <maedasyu625@gmail.com>
2023-08-29 00:04:08 +0900

    check

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 mkdir build
 cd build
 cmake -D TP_DIR=/mnt/d/lib/TextParser \
--D CMAKE_INSTALL_PREFIX=/mnt/d/work/O17/3D_simulation/bin \
+-D CMAKE_INSTALL_PREFIX=/mnt/d/work/O17/3D_voxel_diffusion/bin \
 -D CMAKE_EXPORT_COMPILE_COMMANDS=ON \
 ..
 
